18fe0eed7762d13948a0ddf7938d48b5d0a4c75c
gregp <gperlman27@gmail.com>
2018-06-10 09:15:37 -0700

    get character and iso map in the game

--- Character.lua
+++ Character.lua
@@ -1,8 +1,10 @@
-Character = Class{}
+Character = Class {}
 
-function Character:init(x, y)
+function Character:init(x, y, width, height)
     self.x = x
     self.y = y
+    self.width = width
+    self.height = height
 
     -- these variables are for keeping track of our velocity on both the
     -- X and Y axis, since the Character can move in two dimensions
@@ -18,5 +20,6 @@ function Character:update(dt)
 end
 
 function Character:render()
-    love.graphics.draw(self.sprite, self.x, self.y)
+    love.graphics.draw(self.sprite, self.x, self.y, self.width, self.height)
+    -- love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
 end
--- main.lua
+++ main.lua
@@ -1,37 +1,64 @@
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
+WINDOW_WIDTH = 1920
+WINDOW_HEIGHT = 1080
 
-Class = require 'class'
+Class = require "class"
 
-require 'models/Character'
+require "models/Character"
 
 PADDLE_SPEED = 200
 
 function love.load()
-    love.window.setTitle('Game')
+    grass = love.graphics.newImage("assets/test.png")
 
-    char = Character(WINDOW_WIDTH / 2 - 2, WINDOW_HEIGHT / 2 - 2, 4, 4)
+    grid_x = 300
+    grid_y = 300
+
+    block_width = grass:getWidth()
+    block_height = grass:getHeight()
+    block_depth = block_height / 2
+
+    grid_size = 20
+    grid = {}
+    for x = 1, grid_size do
+        grid[x] = {}
+        for y = 1, grid_size do
+            grid[x][y] = 1
+        end
+    end
+
+    love.window.setTitle("Game")
+
+    char = Character(50, 50)
 end
 
 function love.draw()
-    love.graphics.printf(
-        'Hello Pong!',          -- text to render
-        0,                      -- starting X (0 since we're going to center it based on width)
-        WINDOW_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
-        WINDOW_WIDTH,           -- number of pixels to center within (the entire screen here)
-        'center')               -- alignment mode, can be 'center', 'left', or 'right'
+    for x = 1, grid_size do
+        for y = 1, grid_size do
+            love.graphics.draw(
+                grass,
+                grid_x + ((y - x) * (block_width / 2)),
+                grid_y + ((x + y) * (block_depth / 2)) - (block_depth * (grid_size / 2))
+            )
+        end
+    end
 
-        char:render()
+    char:render()
 end
 
 function love.update(dt)
-    if love.keyboard.isDown('w') then
+    if love.keyboard.isDown("w") then
         char.dy = -PADDLE_SPEED
-    elseif love.keyboard.isDown('s') then
+    elseif love.keyboard.isDown("s") then
         char.dy = PADDLE_SPEED
+    elseif love.keyboard.isDown("a") then
+        char.dx = -PADDLE_SPEED
+    elseif love.keyboard.isDown("d") then
+        char.dx = PADDLE_SPEED
     else
         char.dy = 0
+        char.dx = 0
     end
+    
 
     char:update(dt)
 end
